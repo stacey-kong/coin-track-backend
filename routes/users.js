4cc27686624ac2fb835db957b6cd5c6d70405ca2
stacey <727@stlnd.net>
2021-02-26 16:40:53 +0800

    Add two routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+const rounds = 10;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/login", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/signup", (req, res) => {
+  bcrypt.hash(req.body.password, rounds, (error, hash) => {
+    if (error) res.status(500).json(error);
+    else {
+      const newUser = User({ username: req.body.username, password: hash });
+      newUser
+        .save()
+        .then((user) => {
+          res.status(200).json(user);
+        })
+        .catch((error) => {
+          res.status(500).json(error);
+        });
+    }
+  });
 });
 
 module.exports = router;
