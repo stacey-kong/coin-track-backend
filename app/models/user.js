ab94ee3f3bd3e01620d6f535d9867c1cf06cce8e
stacey <727@stlnd.net>
2021-03-01 15:44:33 +0800

    revise users model

--- app/models/user.js
+++ app/models/user.js
@@ -1,14 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const model = mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
 });
 
-module.exports = new mongoose.model("User", model)
+module.exports = new mongoose.model("User", model);
