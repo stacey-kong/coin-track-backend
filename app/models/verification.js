e4a648331420b163857bdf03965e496290c0f1c5
stacey <727@stlnd.net>
2021-03-01 15:45:15 +0800

    add verification model

---
+++ app/models/verification.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const verificationSchema = new mongoose.Schema({
+  token: {
+    type: String,
+    maxlength: 255,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    maxlength: 255,
+  },
+});
