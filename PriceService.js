047aa2e68d4195237427482e8d00db3847ae950d
stacey <727@stlnd.net>
2021-03-24 11:17:26 +0800

    seperate return price service and add average price list and binance price list service

---
+++ PriceService.js
@@ -0,0 +1,76 @@
+const CoinInstance = require("./app/models/coininstance");
+const Coin = require("./app/models/coin");
+const coin = require("./app/models/coin");
+
+exports.binancePriceList = async function () {
+  let ftxPriceList;
+  ftxPriceList = await Coin.find({})
+    .then(async (docs) => {
+      let priceList = [];
+      for (let i = 0; i < docs.length; i++) {
+        const query = { coin: docs[i]._id, exchange: "FTX" };
+        const price = await CoinInstance.findOne(query)
+          .then((res) => {
+            return res.price;
+          })
+          .catch((err) => console.log(err));
+        let data = { name: docs[i].name, price: price };
+        priceList.push(data);
+      }
+      return priceList;
+    })
+    .catch((err) => console.log(err));
+  return binancePriceList;
+};
+
+exports.ftxPriceList = async function () {
+  let ftxPriceList;
+  ftxPriceList = await Coin.find({})
+    .then(async (docs) => {
+      let priceList = [];
+      for (let i = 0; i < docs.length; i++) {
+        const query = { coin: docs[i]._id, exchange: "FTX" };
+        const price = await CoinInstance.findOne(query)
+          .then((res) => {
+            return res.price;
+          })
+          .catch((err) => console.log(err));
+        let data = { name: docs[i].name, price: price };
+        priceList.push(data);
+      }
+      return priceList;
+    })
+    .catch((err) => console.log(err));
+  return ftxPriceList;
+};
+
+exports.averagePriceList = async function () {
+  let averagePrice;
+  averagePrice = await Coin.find({})
+    .then(async (docs) => {
+      let priceList = [];
+      for (let i = 0; i < docs.length; i++) {
+        let ftxPrice = await CoinInstance.findOne({
+          coin: docs[i]._id,
+          exchange: "FTX",
+        }).then((res) => {
+          return res.price;
+        });
+        let binancePrice = await CoinInstance.findOne({
+          coin: docs[i]._id,
+          exchange: "Binance",
+        }).then((res) => {
+          return res.price;
+        });
+        let data = {
+          name: docs[i].name,
+          price: (ftxPrice + binancePrice) / 2,
+        };
+        // console.log(data);
+        priceList.push(data);
+      }
+      return priceList;
+    })
+    .catch((err) => console.log(err));
+  return averagePrice;
+};
