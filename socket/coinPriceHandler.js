2903a1ea02bcb3502f18a6cf1f60dabf2a7493b7
stacey <727@stlnd.net>
2021-06-03 11:39:50 +0800

    fire push coin price when io connection is established

--- socket/coinPriceHandler.js
+++ socket/coinPriceHandler.js
@@ -1,33 +1,16 @@
 const socker = require("./sockerController");
-const {
-  binancePriceList,
-  ftxPriceList,
-  averagePriceList,
-  checkCoinprice,
-} = require("./renderPrice");
+const { checkCoinprice } = require("./renderPrice");
 
 module.exports = async (io, socket) => {
-  const pushAveragePrice = (payload) => {
-    const pushAveragePriceOnce = async () => {
-      const averageprice = await averagePriceList(payload);
-      socket.emit("allPrice", averageprice);
-    };
-    pushAveragePriceOnce()
-    setInterval(pushAveragePriceOnce, 3000);
-  };
-
   const pushCoinPrice = (coin) => {
     const pushCoinPriceOnce = async () => {
       const coinprice = await checkCoinprice(coin);
       socket.emit(`${coin}`, coinprice);
     };
-    pushCoinPriceOnce()
+    pushCoinPriceOnce();
     setInterval(pushCoinPriceOnce, 3000);
   };
-  socket.on("averageprice", (payload) => {
-    console.log(payload);
-    pushAveragePrice(payload);
-  });
+
   socket.on("coinprice", (data) => {
     console.log(data);
     pushCoinPrice(data);
--- socket/sockerController.js
+++ socket/sockerController.js
@@ -3,6 +3,28 @@ const coinPriceHandler = require("./coinPriceHandler");
 const subscriptionHandler = require("./subscriptionHandler");
 const coinTrackingListHandler = require("./coinTrackingListHandler");
 const lendingInterestHandler = require("./lendingInterestHandler");
+// const {pushAveragePrice}=require("./coinPriceHandler")
+const { PriceList } = require("../BackendService/index");
+
+
+
+
+const pushAveragePrice = (io) => {
+  let i = 1;
+  const pushAveragePriceOnce = () => {
+    io.to('room1').emit(PriceList);
+    console.log(`send${i}`);
+    i = i + 1;
+  };
+  pushAveragePriceOnce();
+  updatePriceList();
+  function updatePriceList() {
+    setTimeout(async function () {
+      pushAveragePriceOnce();
+      updatePriceList();
+    }, 1000);
+  }
+};
 
 exports.socker = (server) => {
   const io = socketio(server, {
@@ -13,6 +35,7 @@ exports.socker = (server) => {
   });
 
   const onConnection = (socket) => {
+    socket.join(`room1`)
     console.log("Client Connected");
     coinPriceHandler(io, socket);
     subscriptionHandler(io, socket);
@@ -21,5 +44,10 @@ exports.socker = (server) => {
   };
 
   io.on("connection", onConnection);
+  io.on("disconnect", () => {
+    console.log("disconnect");
+    io.close();
+  });
+  pushAveragePrice(io)
   return io;
 };
